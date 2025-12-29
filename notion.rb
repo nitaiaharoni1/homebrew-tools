2cc354a85a9553f46112fd028905658d51ee20c0
nitaiaharoni <nitaiaharoni1@gmail.com>
2025-12-29 15:58:47 +0200

    Add Slack and Notion CLI formulas

---
+++ notion.rb
@@ -0,0 +1,47 @@
+# Homebrew formula for Notion CLI
+# Formula name: notion
+# Tap: nitaiaharoni1/tools
+
+class Notion < Formula
+  desc "Powerful command-line interface for Notion built with pure bash"
+  homepage "https://github.com/nitaiaharoni1/notion-cli"
+  url "https://github.com/nitaiaharoni1/notion-cli/archive/refs/heads/main.tar.gz"
+  version "1.0.0"
+  sha256 "placeholder_sha256_will_be_updated_on_release"
+  license "MIT"
+  head "https://github.com/nitaiaharoni1/notion-cli.git", branch: "main"
+
+  depends_on "bash" => :build
+  depends_on "curl"
+  depends_on "python3"
+
+  def install
+    bin.install "notion-cli.sh" => "notion"
+    # Make sure the script is executable
+    chmod 0755, bin/"notion"
+  end
+
+  test do
+    # Test that the script exists and is executable
+    assert_match "Notion CLI", shell_output("#{bin}/notion help 2>&1", 0)
+  end
+
+  def caveats
+    <<~EOS
+      Notion CLI has been installed!
+
+      To get started:
+      1. Run setup: notion init
+
+      The 'notion init' command will guide you through:
+      - Getting a Notion integration token
+      - Storing it securely
+      - Testing the connection
+
+      Then use: notion help
+
+      Note: Make sure to share pages/databases with your integration in Notion.
+    EOS
+  end
+end
+
---
+++ slack.rb
@@ -0,0 +1,47 @@
+# Homebrew formula for Slack CLI
+# Formula name: slack
+# Tap: nitaiaharoni1/tools
+
+class Slack < Formula
+  desc "Powerful command-line interface for Slack built with pure bash"
+  homepage "https://github.com/nitaiaharoni1/slack-cli"
+  url "https://github.com/nitaiaharoni1/slack-cli/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "a052fa5e9add669654163945119c315b356e38927c37a61519ea875d6e2f6d00"
+  license "MIT"
+  head "https://github.com/nitaiaharoni1/slack-cli.git", branch: "main"
+
+  depends_on "bash" => :build
+  depends_on "curl"
+  depends_on "python3"
+
+  def install
+    bin.install "slack-cli.sh" => "slack-chat"
+    # Make sure the script is executable
+    chmod 0755, bin/"slack-chat"
+  end
+
+  test do
+    # Test that the script exists and is executable
+    assert_match "Slack CLI", shell_output("#{bin}/slack-chat help 2>&1", 1)
+  end
+
+  def caveats
+    <<~EOS
+      Slack CLI has been installed!
+
+      To get started:
+      1. Run setup: slack-chat init
+
+      The 'slack-chat init' command will guide you through:
+      - Getting a Slack token
+      - Storing it securely
+      - Testing the connection
+
+      Then use: slack-chat help
+
+      Note: This installs as 'slack-chat' to coexist with official Slack CLI.
+      No need to source in your shell config - it works as a standalone command!
+    EOS
+  end
+end
+
