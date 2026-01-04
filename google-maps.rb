e248040c8300758c53e86daa4bff2bb5064075c2
nitaiaharoni <nitaiaharoni1@gmail.com>
2026-01-04 12:15:18 +0200

    Update SHA256 for v1.1.0

--- google-maps.rb
+++ google-maps.rb
@@ -7,7 +7,7 @@ class GoogleMaps < Formula
   homepage "https://github.com/nitaiaharoni1/google-maps-cli"
   url "https://github.com/nitaiaharoni1/google-maps-cli/archive/refs/tags/v1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "6b7a4e393f2a5e69daa085434200596a717e320c4a96de6f045af5616fc1fe2b"
+  sha256 "a40ffbd016d39bc28d6b09f1220ed28c3bf44d0c81b380767aa6f15a8294aafe"
   license "MIT"
   head "https://github.com/nitaiaharoni1/google-maps-cli.git", branch: "main"
 
