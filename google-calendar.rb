7d08a4dbd6987673150cec224db9a65d16be2159
nitaiaharoni <nitaiaharoni1@gmail.com>
2026-01-06 22:51:56 +0200

    Update google-calendar-cli to v1.2.0 - Find Time feature

--- google-calendar.rb
+++ google-calendar.rb
@@ -5,9 +5,9 @@
 class GoogleCalendar < Formula
   desc "Command-line interface for Google Calendar"
   homepage "https://github.com/nitaiaharoni1/google-calendar-cli"
-  url "https://github.com/nitaiaharoni1/google-calendar-cli/archive/refs/tags/v1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "7df14166d1574a095e3590e1d03d9d033510c366a87876b94045c08071fb9f91"
+  url "https://github.com/nitaiaharoni1/google-calendar-cli/archive/refs/tags/v1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "74d714fab15fc9e86b5cef11cfd02b7d392b82e896ded8c8fc18d5b26c1926a8"
   license "MIT"
   head "https://github.com/nitaiaharoni1/google-calendar-cli.git", branch: "main"
 
