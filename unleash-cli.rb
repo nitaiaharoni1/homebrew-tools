493e25ec76af9cdc81a102095f5cac248470e33a
nitaiaharoni <nitaiaharoni1@gmail.com>
2026-01-07 12:34:29 +0200

    fixes SHA256 for unleash-cli v0.1.2

--- unleash-cli.rb
+++ unleash-cli.rb
@@ -3,7 +3,7 @@ class UnleashCli < Formula
   desc "Comprehensive CLI tool for managing Unleash Cloud feature flags"
   homepage "https://github.com/nitaiaharoni1/unleash-cli"
   url "https://github.com/nitaiaharoni1/unleash-cli/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "771dbaba9c458f1c07e015ea667304b0eb4cc1a514fe98ff88919f7ebd99589a"
+  sha256 "b2b86e8a21ac2b888dd244e3e2f16e6f2c8aeeda20923c96fb878ee0a242892a"
   license "MIT"
 
   depends_on "python@3.11"
